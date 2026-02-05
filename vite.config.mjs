3906eff2e45015bf0f8f022907bc4980641105af
Pablo Rendón Vásquez <103075077+prendonv@users.noreply.github.com>
2026-02-05 06:27:25 -0500

    Update and rename vite.config.js to vite.config.mjs

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  export default defineConfig({
-    base: './',
-    plugins: [react()],
-  })
+  plugins: [react()],
+  base: '/',
+})
